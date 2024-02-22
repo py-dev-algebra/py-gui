56870270e029e883477d8d4a02a732bb880f8271
Krunoslav Lujic <python.developer.edukacija@gmail.com>
2024-02-22 19:25:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,20 +6,19 @@ from event_handlers.btn_get_customers_handler import on_click
 main_window = tk.Tk()
 
 main_window.title('Algebra - Python desktop app')
-main_window.geometry('600x400')
+main_window.geometry('600x400') # Pocetna velicina glavnog prozora
 
 
 # Naziv konstruirati od widget_funkcija
 lbl_title = tk.Label(main_window,
                      text='NASLOV',
                      font=('Segoe UI', 18))
-lbl_title.pack()
+lbl_title.pack(padx=15, pady=15)
 
 btn_get_customers = tk.Button(main_window,
                        text='Dohvati sve korisnike',
                        font=('Segoe UI', 15),
-                       command=on_click).pack()
-
+                       command=on_click).pack(padx=15, pady=15)
 
 
 main_window.mainloop()
